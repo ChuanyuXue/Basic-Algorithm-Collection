4dc23a4db3fa0637b4f6ef4d7dc8558786bc1292
Chuanyu Xue <cs_xcy@126.com>
2020-09-21 23:42:12 +0800

    add leetcode 43 44

---
+++ 43.字符串相乘.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=43 lang=python3
+#
+# [43] 字符串相乘
+#
+
+# @lc code=start
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        return str(eval(num1) * eval(num2))
+# @lc code=end
+
